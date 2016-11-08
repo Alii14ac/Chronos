76bca6c45bbfc50ba8709a2de0b335946306c36c
alexanderlindkjaer <alindkjaer@gmail.com>
2016-11-08 17:07:21 +0100

    basic database read/write + signup database add

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,39 @@
 angular.module('chronos.controllers', [])
 
 //LOGIN CONTROLLER
-.controller('LoginCtrl', function($scope, $state, $ionicPopup, Auth){
-  $scope.data={};
+	.controller('LoginCtrl', function($scope, $state, $ionicPopup, Auth){
+	$scope.data={};
 
 	//AUTH login
-  $scope.login=function(){
-    Auth.signInWithEmailAndPassword($scope.data.email, $scope.data.password)
-    .then(function(authData){
-        console.log(authData);
-        $state.go('tab.timers');
-      }).catch(function(error) {
-        console.log(error);
-        varalertPopup=$ionicPopup.alert({
-        title: 'Login failed!',
-        template: 'Please check your credentials!'
-    });
-  });
-  };
+	$scope.login=function(){
+		Auth.signInWithEmailAndPassword($scope.data.email, $scope.data.password)
+			.then(function(authData){
+			console.log(authData);
+			$state.go('tab.timers');
+		}).catch(function(error) {
+			console.log(error);
+			varalertPopup=$ionicPopup.alert({
+				title: 'Login failed!',
+				template: 'Please check your credentials!'
+			});
+		});
+	};
 
 	//go to signup
 	$scope.signup=function(){
 		$state.go('signup');
 	}
+
+	$scope.test = function(){
+		console.log("test function reached!")
+	};
+
 })
-	//LOGIN END
+//LOGIN END
 
 //SIGNUP CONTROLLER
-.controller('SignupCtrl', function($scope, $state, $ionicPopup, $ionicPopup, Auth){
-  $scope.data={};
+	.controller('SignupCtrl', function($scope, $state, $ionicPopup, $ionicPopup, Auth, CurrentUser){
+	$scope.data={};
 
 	$scope.signup = function(){
 
@@ -37,24 +42,28 @@ angular.module('chronos.controllers', [])
 		//TODO: check for repeat password
 
 		Auth.createUserWithEmailAndPassword(email, password)
-    .then(function(authData){
+			.then(function(authData){
+			//On sucess, add user to database through uid
+			console.log(authData);
+			console.log("user created")
+			firebase.database().ref("users").child(authData.uid).update({
+				email: authData.email
+		});
 
-        console.log(authData);
-				console.log("user created")
-				varalertPopup=$ionicPopup.alert({
-        title: 'Succes!',
-        template: 'User created'
-    });
+			varalertPopup=$ionicPopup.alert({
+				title: 'Succes!',
+				template: 'User created'
+			});
 
-        $state.go('login');
+			$state.go('login');
 
-      }).catch(function(error) {
-        console.log(error);
-        varalertPopup=$ionicPopup.alert({
-        title: 'Sign up failed!',
-        template: 'Please check your credentials!'
-    });
-  });
+		}).catch(function(error) {
+			console.log(error);
+			varalertPopup=$ionicPopup.alert({
+				title: 'Sign up failed!',
+				template: 'Please check your credentials!'
+			});
+		});
 	};
 
 
@@ -62,120 +71,137 @@ angular.module('chronos.controllers', [])
 })
 //SIGNUP END
 
-.controller('DashCtrl', function($scope, $timeout, Foods) {
-  // $scope.plotData = Foods.getPlotData();
+	.controller('DashCtrl', function($scope, $timeout, Foods) {
+	// $scope.plotData = Foods.getPlotData();
 
-  // $scope.refreshDiaries=function() {
-  //   $timeout(function() {
-  //   $scope.plotData=Foods.getPlotData();
-  //   $scope.$broadcast('scroll.refreshComplete');
-  //   }, 1000);
-  // }
+	// $scope.refreshDiaries=function() {
+	//   $timeout(function() {
+	//   $scope.plotData=Foods.getPlotData();
+	//   $scope.$broadcast('scroll.refreshComplete');
+	//   }, 1000);
+	// }
 
-  firebase.database().ref('diaries').on('value', function(snapshot) {
-    var tmp=snapshot.val();
-    var diariesArray=[];
-    for(var i in tmp){
-      diariesArray.push(tmp[i]);
-    }
-    $scope.plotData = Foods.getPlotData(diariesArray);
-  });
-// $
+	firebase.database().ref('diaries').on('value', function(snapshot) {
+		var tmp=snapshot.val();
+		var diariesArray=[];
+		for(var i in tmp){
+			diariesArray.push(tmp[i]);
+		}
+		$scope.plotData = Foods.getPlotData(diariesArray);
+	});
+	// $
 })
 
-.controller('FoodsCtrl', function($scope, $ionicFilterBar, $ionicPopup, $state, Foods) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  var filterBarInstance;
-  $scope.showFilterBar=function () {
-    filterBarInstance = $ionicFilterBar.show({
-    items:$scope.foods,
-    update:function (filteredItems, filterText) {
-      $scope.foods=filteredItems;
-        if(filterText) {
-          console.log(filterText);
-        }
-      }
-    });
-  };
-  $scope.add=function(view){
-    $state.go(view);
-  }
-  $scope.showPopup=function(food) {
-    $scope.newDiary = {
-      food: {
-        id: food.$id,
-        cat: food.cat,
-        calories: food.calories,
-        name: food.name,
-        img: food.img
-      }
-    };
-    $scope.data={};
-    // An elaborate, custom popup
-    var myPopup=$ionicPopup.show({
-      templateUrl: 'templates/foods-popup.html',
-      title: 'Make a Calories Diary',
-      subTitle: 'How Many Ozsand When did you eat '+food.name,
-      scope: $scope,
-      buttons: [
-      {text: 'Cancel'},
-      {text: '<b>Confirm</b>',
-      type: 'button-positive',
-      onTap: function(e) {
-        if(!$scope.data.amount||!$scope.data.date||!$scope.data.time) {
-          //don't allow the user to close unless he enters wifipassword
-          e.preventDefault();
-        } else{
-          var datetime=$scope.data.date.getFullYear()+'-'
-          +("0"+($scope.data.date.getMonth()+1)).slice(-2)+'-'
-          +("0"+$scope.data.date.getDate()).slice(-2)+'T'
-          +("0"+$scope.data.time.getHours()).slice(-2)+':'
-          +("0"+$scope.data.time.getMinutes()).slice(-2)+':'
-          +("0"+$scope.data.time.getSeconds()).slice(-2);
-          $scope.newDiary.amount=$scope.data.amount;
-          $scope.newDiary.date=datetime;
-          Foods.addDiary($scope.newDiary);
-          return $scope.data;
-        }
-      }
-      }
-    ]
-    });
-  }
-  $scope.foods = Foods.all();
-  $scope.remove = function(food) {
-    Foods.remove(food);
-  };
-})
-.controller('AddCtrl', function($scope, $state, Foods){
-  $scope.foods=Foods.all();
-  $scope.newFood={
-    id:$scope.foods.length,
-    name:'',
-    cat:'',
-    img:'',
-    calories: 0
-  }
-  $scope.confirm=function () {
-    Foods.add($scope.newFood);
-    $scope.close();
-  };
-  $scope.close=function() {
-    $state.go('tab.foods');
-  };
-})
-.controller('FoodDetailCtrl', function($scope, $stateParams, Foods) {
-  $scope.food = Foods.get($stateParams.foodId);
+	.controller('TimersCtrl', function($scope, $ionicFilterBar, $ionicPopup, $state, Foods, CurrentUser) {
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
+
+	$scope.testWrite = function(){
+		console.log("testWrite function reached");
+		$scope.testWrite.data = {};
+		$scope.testWrite.data.name = "NameTest1"
+		$scope.testWrite.data.email = "Email@test.dk1"
+		$scope.testWrite.writeUserData = function() {
+			console.log("data set");
+			firebase.database().ref('/users/email').set({
+				username1: $scope.data.testWrite.name,
+				email1: $scope.data.testWrite.email
+			});
+		};
+	};
+
+	$scope.test = function(){
+		console.log("test function reached!");
+	};
+
+
+
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+	.controller('FireCtrl', function($scope, $ionicFilterBar, $ionicPopup, $state, Foods, CurrentUser) {
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
+
+	$scope.testWrite = function(){
+		console.log("testWrite function reached");
+		$scope.testWrite.data = {};
+		$scope.testWrite.data.name = "NameTest1"
+		$scope.testWrite.data.email = "Email@test.dk1"
+		$scope.testWrite.writeUserData = function() {
+			console.log("data set");
+			firebase.database().ref('/users/email').set({
+				username1: $scope.data.testWrite.name,
+				email1: $scope.data.testWrite.email
+			});
+		};
+	};
+
+
+	$scope.testRead = function(){
+		console.log('test read function reached');
+		var starCountRef = firebase.database().ref('users/');
+		console.log(starCountRef);
+		starCountRef.on('value', function(snapshot) {
+			updateStarCount(postElement, snapshot.val());
+		});
+	};
+
+	$scope.test = function(){
+		console.log("test function reached!")
+	};
+
+	$scope.currentUser = function(){
+		console.log("current user function reached");
+		var current = CurrentUser.uid;
+		console.log(current);
+
+	};
+
+	$scope.writeUserId = function() {
+		var currentUser = CurrentUser.uid;
+		console.log("writeUserId function reached");
+		firebase.database().ref("users").child(CurrentUser.uid).update({
+				email: CurrentUser.email
+		});
+	}
+
+	})
+
+
+
+		.controller('AddCtrl', function($scope, $state, Foods){
+			$scope.foods=Foods.all();
+			$scope.newFood={
+				id:$scope.foods.length,
+				name:'',
+				cat:'',
+				img:'',
+				calories: 0
+			}
+			$scope.confirm=function () {
+				Foods.add($scope.newFood);
+				$scope.close();
+			};
+			$scope.close=function() {
+				$state.go('tab.foods');
+			};
+		})
+			.controller('FoodDetailCtrl', function($scope, $stateParams, Foods) {
+			$scope.food = Foods.get($stateParams.foodId);
+		})
+
+			.controller('AccountCtrl', function($scope) {
+			$scope.settings = {
+				enableFriends: true
+			};
+		});
--- www/js/services.js
+++ www/js/services.js
@@ -1,103 +1,123 @@
- angular.module('chronos.services', [])
+angular.module('chronos.services', [])
 
-.factory('Auth', function(){
-  var config = {
-    apiKey: "AIzaSyDSJ3nHOVn-frvyhB2brmS0kdOu57ci0Fo",
-    authDomain: "chronos-c63b2.firebaseapp.com",
-    databaseURL: "https://chronos-c63b2.firebaseio.com",
-    storageBucket: "chronos-c63b2.appspot.com",
-    messagingSenderId: "1054527482628"
-  };
-  firebase.initializeApp(config);
-  return firebase.auth();
+	.factory('Auth', function(){
+	var config = {
+		apiKey: "AIzaSyDSJ3nHOVn-frvyhB2brmS0kdOu57ci0Fo",
+		authDomain: "chronos-c63b2.firebaseapp.com",
+		databaseURL: "https://chronos-c63b2.firebaseio.com",
+		storageBucket: "chronos-c63b2.appspot.com",
+		messagingSenderId: "1054527482628"
+	};
+	firebase.initializeApp(config);
+	return firebase.auth();
 })
-.factory('Foods', function($firebaseArray) {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var foods = $firebaseArray(firebase.database().ref('foods'));
-  var diaries = $firebaseArray(firebase.database().ref('diaries'));
 
-  function compare(a,b) {
-    var d1 = new Date(a.date);
-    var d2 = new Date(b.date);
-    if(d1.getTime() <d2.getTime())
-      return-1;
-    if(d1.getTime() >d2.getTime())
-      return 1;
-    return 0;
-  }
+	.factory('CurrentUser', function(){
 
-  function createPlotData(diaries){
-    var startDate= new Date(diaries[0].date.split('T')[0]);
-    var endtDate= new Date(diaries[diaries.length-1].date.split('T')[0]);
-    var counter =0;
-    var plotData={
-      labels: [],
-      series: ['Intake'],
-      data: []
-    };
-    while(startDate.getTime()<=endtDate.getTime()){
-      var currentDate= new Date(diaries[counter].date);
-      var tmp=0;
-      var timeDiff=Math.abs(currentDate.getTime() - startDate.getTime());
-      var diffDays=timeDiff/ (1000*3600*24);
-      plotData.labels.push(startDate.toDateString());
-      if(currentDate.getTime() > startDate.getTime() && diffDays>=1){
-        plotData.data.push(0);
-        startDate.setDate(startDate.getDate()+1);
-        continue;
-      }
-      while(diffDays<1){
-        tmp+=parseFloat(diaries[counter].amount) *parseInt(diaries[counter].food.calories);
-        counter +=1;
-        if(counter >= diaries.length){
-          break;
-        }
-        currentDate = new Date(diaries[counter].date);
-        timeDiff=Math.abs(currentDate.getTime() -startDate.getTime());
-        diffDays=timeDiff/ (1000*3600*24);
-      }
-      plotData.data.push(tmp);
-      startDate.setDate(startDate.getDate()+1);
-    }
-    return plotData;
-  }
+	var user = firebase.auth().currentUser;
+	var name, email, photoUrl, uid;
 
-  return {
-    add: function(newFood) {
-      var updates ={};
-      var newId=firebase.database().ref().child('foods').push().key;
-      updates['/foods/'+newId] = newFood;
-      firebase.database().ref().update(updates);
-    },
-    all: function() {
-      return foods;
-    },
-    remove: function(food) {
-      firebase.database().ref('foods/'+food.$id).remove();
-    },
-    get: function(foodId) {
-      for(vari=0; i<foods.length; i++) {
-        if(foods[i].$id === foodId) {
-          return foods[i];
-        }
-      }
-      return null;
-    },
-    getPlotData: function(diariesArray) {
-      diariesArray.sort(compare);
-      return createPlotData(diariesArray);
-    },
-    addDiary: function(diary){
-      var updates = {};
-      var newId = firebase.database().ref().child('diaries').push().key;
-      updates['/diaries/' + newId] = diary;
-      firebase.database().ref().update(updates);
-    },
-    getDiaries: function() {
-      diaries.sort(compare);
-      return diaries;
-    }
-  };
-});
+	if (user != null) {
+		//name = user.displayName;
+		email = user.email;
+		//photoUrl = user.photoURL;
+		uid = user.uid;
+	}
+
+	return user;
+
+	})
+
+
+
+		.factory('Foods', function($firebaseArray) {
+			// Might use a resource here that returns a JSON array
+
+			// Some fake testing data
+			var foods = $firebaseArray(firebase.database().ref('foods'));
+			var diaries = $firebaseArray(firebase.database().ref('diaries'));
+
+			function compare(a,b) {
+				var d1 = new Date(a.date);
+				var d2 = new Date(b.date);
+				if(d1.getTime() <d2.getTime())
+					return-1;
+				if(d1.getTime() >d2.getTime())
+					return 1;
+				return 0;
+			}
+
+			function createPlotData(diaries){
+				var startDate= new Date(diaries[0].date.split('T')[0]);
+				var endtDate= new Date(diaries[diaries.length-1].date.split('T')[0]);
+				var counter =0;
+				var plotData={
+					labels: [],
+					series: ['Intake'],
+					data: []
+				};
+				while(startDate.getTime()<=endtDate.getTime()){
+					var currentDate= new Date(diaries[counter].date);
+					var tmp=0;
+					var timeDiff=Math.abs(currentDate.getTime() - startDate.getTime());
+					var diffDays=timeDiff/ (1000*3600*24);
+					plotData.labels.push(startDate.toDateString());
+					if(currentDate.getTime() > startDate.getTime() && diffDays>=1){
+						plotData.data.push(0);
+						startDate.setDate(startDate.getDate()+1);
+						continue;
+					}
+					while(diffDays<1){
+						tmp+=parseFloat(diaries[counter].amount) *parseInt(diaries[counter].food.calories);
+						counter +=1;
+						if(counter >= diaries.length){
+							break;
+						}
+						currentDate = new Date(diaries[counter].date);
+						timeDiff=Math.abs(currentDate.getTime() -startDate.getTime());
+						diffDays=timeDiff/ (1000*3600*24);
+					}
+					plotData.data.push(tmp);
+					startDate.setDate(startDate.getDate()+1);
+				}
+				return plotData;
+			}
+
+			return {
+				add: function(newFood) {
+					var updates ={};
+					var newId=firebase.database().ref().child('foods').push().key;
+					updates['/foods/'+newId] = newFood;
+					firebase.database().ref().update(updates);
+				},
+				all: function() {
+					return foods;
+				},
+				remove: function(food) {
+					firebase.database().ref('foods/'+food.$id).remove();
+				},
+				get: function(foodId) {
+					for(vari=0; i<foods.length; i++) {
+						if(foods[i].$id === foodId) {
+							return foods[i];
+						}
+					}
+					return null;
+				},
+				getPlotData: function(diariesArray) {
+					diariesArray.sort(compare);
+					return createPlotData(diariesArray);
+				},
+				addDiary: function(diary){
+					var updates = {};
+					var newId = firebase.database().ref().child('diaries').push().key;
+					updates['/diaries/' + newId] = diary;
+					firebase.database().ref().update(updates);
+				},
+				getDiaries: function() {
+					diaries.sort(compare);
+					return diaries;
+				}
+			};
+		});
